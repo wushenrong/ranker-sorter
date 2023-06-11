f53c2e4cb85881a541677c91e0ba80f4c629cf20
Samuel Wu <twopizza9621536@gmail.com>
2023-06-11 13:01:41 -0400

    [feat]: Upload to pages
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import postcssNesting from 'postcss-nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ranker-sorter/',
   plugins: [
     react(),
     optimizeCssModules(),
