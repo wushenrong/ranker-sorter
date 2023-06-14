3cb2b3a24804fed134cb546101557982ae6b466e
Samuel Wu <twopizza9621536@gmail.com>
2023-06-14 14:44:41 -0400

    fix: case-sensitivity files (#15)
    * chore: fix code convention, formatting, and enable ext import
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * fix: update typescript compiler
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * Rename App.tsx to app.tsx
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * Rename Chart.module.css to chart.module.css
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * Rename Chart.tsx to chart.tsx
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * Rename Loading.tsx to loading.tsx
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * Rename Ranker.module.css to ranker.module.css
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * Rename Ranker.tsx to ranker.tsx
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    * Rename Results.tsx to results.tsx
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>
    
    ---------
    
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

---
+++ src/app.tsx
@@ -0,0 +1,45 @@
+/*! SPDX-FileCopyrightText: 2023 Samuel Wu
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import { Suspense, lazy, useState } from 'react'
+
+import LoadList from './components/load-list'
+import Loading from './components/loading'
+
+import type { PlayerList } from './hooks/useEloSystem'
+import type { RankerResults } from './components/results'
+
+const Ranker = lazy(async () => await import('./components/ranker'))
+const Results = lazy(async () => await import('./components/results'))
+
+export default function App (): JSX.Element {
+  const [players, setPlayer] = useState<PlayerList | null>(null)
+  const [results, setResults] = useState<RankerResults | null>(null)
+
+  const getPlayers = (playerList: PlayerList | null): void => {
+    setPlayer(playerList)
+  }
+  const getResults = (results: RankerResults | null): void => {
+    setResults(results)
+  }
+
+  if (players == null) return <LoadList callback={getPlayers} />
+
+  if (results == null) {
+    return (
+      <Suspense fallback={<Loading />}>
+        <Ranker data={players} callback={getResults} />
+      </Suspense>
+    )
+  }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <Results
+        results={results} resetData={getPlayers} resetResults={getResults}
+      />
+    </Suspense>
+  )
+}
---
+++ src/components/loading.tsx
@@ -0,0 +1,8 @@
+/*! SPDX-FileCopyrightText: 2023 Samuel Wu
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+export default function Loading (): JSX.Element {
+  return <p><i>Loading ...</i></p>
+}
